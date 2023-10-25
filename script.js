d8eea4ae7b1aac237b1d91c065b669be4d27bcd9
Elba <elba.duishekeeva16@gmail.com>
2023-10-25 13:59:58 +0600

    first task

---
+++ script.js
@@ -0,0 +1,25 @@
+// let a = 4 
+// let b = 55
+
+// console.log (a+b, a%b, b%a,a/b, b/a, a*b);
+
+// let a = 'Hello'
+// let b = 'Elba'
+
+// console.log (a+' '+b);
+
+// let a='Hello'
+// let b=5
+
+// console.log (a-b);
+
+// let a = 5
+// let b = '5'
+
+// console.log (+b+a);
+
+// let c=2
+// let b
+
+// console.log(c+b)
+
